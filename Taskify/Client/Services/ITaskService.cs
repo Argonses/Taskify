188cdbe22b381b47b8c19b1f5ef7dd035f76f574
resr <vaclavsr@post.cz>
2023-10-14 08:56:44 +0200

    Added Services on client, migration and task table

---
+++ Taskify/Client/Services/ITaskService.cs
@@ -0,0 +1,8 @@
+﻿namespace Taskify.Client.Services
+{
+    public interface ITaskService
+    {
+        List<Taskify.Shared.Task> Tasks { get; set; }
+        Task GetAllTasks();
+    }
+}
---
+++ Taskify/Client/Services/TaskService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+namespace Taskify.Client.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly HttpClient _http;
+
+        public TaskService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Taskify.Shared.Task> Tasks { get; set; } = new List<Taskify.Shared.Task>();
+
+        public async System.Threading.Tasks.Task GetAllTasks()
+        {
+            var result = await _http.GetFromJsonAsync<List<Taskify.Shared.Task>>("api/Tasks");
+            if (result != null)
+            {
+                Tasks = result;
+            }
+        }
+    }
+}
